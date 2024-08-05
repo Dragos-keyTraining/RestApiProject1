cd1837c2093f45f8ec677398cf46169957a4d9fd
Dragos Tanta <dragos.tanta@gmail.com>
2024-08-05 21:27:43 +0300

    curs39 lombok

---
+++ src/main/java/tests/BookingId.java
@@ -0,0 +1,16 @@
+package tests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingId {
+
+	public int bookingid;
+	public Booking booking;
+	
+	
+}
